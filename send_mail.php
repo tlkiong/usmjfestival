18c580b220fdccc05452d2bfa31ad37df32dfdbe
tlkiong <kiong_90@yahoo.com>
2014-09-07 02:00:36 +0800

    added sendgrid
    cannot send php mail on heroku. have to add sendgrid. hopefully it works
    this time

--- send_mail.php
+++ send_mail.php
@@ -1,4 +1,7 @@
 <?php
+
+	require 'vendor/autoload.php';
+
 	/*
 	This first bit sets the email address that you want the form to be submitted to.
 	You will need to change this value to a valid email address that you can access.
@@ -9,7 +12,6 @@
 	This bit sets the URLs of the supporting pages.
 	If you change the names of any of the pages, you will need to change the values here.
 	*/
-	$feedback_page1 = "feedback_form.html";
 	$feedback_page = "jfcontactus.html";
 	$error_page = "error_message.html";
 	$thankyou_page = "thank_you.html";
@@ -19,40 +21,25 @@
 	If you add a form field, you will need to add it here.
 	*/
 	$email_subject = "Message from usmjapanfestival.herokuapp.com website";
-	$from_email_address = $_REQUEST['sender_email_address'] ;
-	$from_contact_number = $_REQUEST['sender_contact_number'];
-	$from_sender_name = $_REQUEST['sender_name'];
+	$senders_email_address = $_REQUEST['sender_email_address'] ;
+	$senders_contact_number = $_REQUEST['sender_contact_number'];
+	$senders_name = $_REQUEST['sender_name'];
 
-	$message = $_REQUEST['sender_message'] ;
+	$senders_message = $_REQUEST['sender_message'] ;
 	$email_body = "From:\n".
 				  "		Name          : $from_sender_name\n".
 				  "		Contact Number: $from_contact_number\n".
 				  "		Message: $message";
 
+	$sendgrid_username = $_ENV['app29290012@heroku.com'];
+	$sendgrid_password = $_ENV['USMJapanFestival'];
 
-	// If the user tries to access this script directly, redirect them to the feedback form,
-	if (!isset($from_email_address)) 
-	{
-		header( "Location: $feedback_page" );
-	}
+	$sendgrid = new SendGrid($sendgrid_username, $sendgrid_password);
+	$email    = new SendGrid\Email();
+	$email->addTo($to)->
+	       setFrom($senders_email_address)->
+	       setSubject($email_subject)->
+	       setText($email_body);
 
-	// If the form fields are empty, redirect to the error page.
-	elseif (empty($from_email_address) || empty($from_contact_number) || empty($from_sender_name) || empty($message)) 
-	{
-		header( "Location: $error_page" );
-	}
-
-	// If we passed all previous tests, send the email then redirect to the thank you page.
-	else 
-	{
-		$mail = mail( $to, $email_subject, $email_body, "From: $from_email_address" );
-		if($mail)
-		{
-			header( "Location: $thankyou_page" );
-		}
-		else
-		{
-			header( "Location: $feedback_page1" );
-		}
-	}
+	$sendgrid->send($email);
 ?>
