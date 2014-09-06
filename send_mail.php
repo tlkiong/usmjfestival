731bc63e99cb6cae2575f2046738d1319745d310
tlkiong <kiong_90@yahoo.com>
2014-09-07 00:45:00 +0800

    aaa

--- send_mail.php
+++ send_mail.php
@@ -1,33 +1,47 @@
 <?php
-	/*
-	This first bit sets the email address that you want the form to be submitted to.
-	You will need to change this value to a valid email address that you can access.
-	*/
-	$to = "usmjfestival@gmail.com";
+/*
+This first bit sets the email address that you want the form to be submitted to.
+You will need to change this value to a valid email address that you can access.
+*/
+$webmaster_email = "kiong90@gmail.com";
 
-	/*
-	This bit sets the URLs of the supporting pages.
-	If you change the names of any of the pages, you will need to change the values here.
-	*/
-	$feedback_page = "jfcontactus.html";
-	$error_page = "error_message.html";
-	$thankyou_page = "thank_you.html";
+/*
+This bit sets the URLs of the supporting pages.
+If you change the names of any of the pages, you will need to change the values here.
+*/
+$feedback_page = "feedback_form.html";
+$error_page = "error_message.html";
+$thankyou_page = "thank_you.html";
 
-	/*
-	This next bit loads the form field data into variables.
-	If you add a form field, you will need to add it here.
-	*/
-	$email_subject = "Message from usmjapanfestival.herokuapp.com website"
-	$from_email_address = $_REQUEST['email_address'] ;
-	$from_contact_number = $_REQUEST['contact_number']
-	$from_sender_name = $_REQUEST['sender_name']
+/*
+This next bit loads the form field data into variables.
+If you add a form field, you will need to add it here.
+*/
+$email_address = $_REQUEST['email_address'] ;
+$comments = $_REQUEST['comments'] ;
 
-	$message = $_REQUEST['message'] ;
-	$email_body = "From:\n".
-				  "		Name          : $from_sender_name\n".
-				  "		Contact Number: $from_contact_number\n".
-				  "		Message: $message";
-
+/*
+The following function checks for email injection.
+Specifically, it checks for carriage returns - typically used by spammers to inject a CC list.
+*/
+function isInjected($str) {
+	$injections = array('(\n+)',
+	'(\r+)',
+	'(\t+)',
+	'(%0A+)',
+	'(%0D+)',
+	'(%08+)',
+	'(%09+)'
+	);
+	$inject = join('|', $injections);
+	$inject = "/$inject/i";
+	if(preg_match($inject,$str)) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
 
 // If the user tries to access this script directly, redirect them to the feedback form,
 if (!isset($_REQUEST['email_address'])) {
@@ -50,21 +64,4 @@ mail( "$webmaster_email", "Feedback Form Results",
   $comments, "From: $email_address" );
 header( "Location: $thankyou_page" );
 }
-
-	// If the user tries to access this script directly, redirect them to the feedback form,
-	/*if (!isset($from_email_address)) {
-	header( "Location: $feedback_page" );
-	}
-
-	// If the form fields are empty, redirect to the error page.
-	elseif (empty($from_email_address) || empty($from_contact_number) || empty($from_sender_name) || empty($message)) {
-	header( "Location: $error_page" );
-	}
-
-	// If we passed all previous tests, send the email then redirect to the thank you page.
-	else {
-	mail( $to, $email_subject, $email_body, $from_email_address );
-
-	header( "Location: $thankyou_page" );
-	}*/
 ?>
