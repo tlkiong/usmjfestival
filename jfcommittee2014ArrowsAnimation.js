d00a50b20220aa59b06a55ff5be392f838c178e4
tlkiong <kiong_90@yahoo.com>
2014-09-22 05:25:30 +0800

    latest pages

---
+++ jfcommittee2014ArrowsAnimation.js
@@ -0,0 +1,313 @@
+function Main1() {
+    document.getElementById('PD_line').style.display = "block";
+    document.getElementById('arrowdown1').style.display = "inline";
+    document.getElementById('arrowdown1').style.marginLeft = "450px";
+
+    document.getElementById('arrowright1').style.display = "none";
+    document.getElementById('arrowright2').style.display = "none";
+    document.getElementById('arrowright3').style.display = "none";
+    document.getElementById('arrowright4').style.display = "none";
+
+    document.getElementById('arrow1').style.display = "none";
+    document.getElementById('arrow2').style.display = "inline";
+    document.getElementById('arrow3').style.display = "inline";
+    document.getElementById('arrow4').style.display = "inline";
+
+    document.getElementById('arrowdown2').style.display = "none";
+    document.getElementById('VPDpublic_line').style.display = "none";
+    document.getElementById('VPDpublic_line_expand').style.display = "none";
+    document.getElementById('sponsorshipPhotos').style.display = "none";
+    document.getElementById('PR_Photos').style.display = "none";
+    document.getElementById('publicityPhotos').style.display = "none";
+
+    document.getElementById('arrowdown3').style.display = "none";
+    document.getElementById('VPDexhibition_line').style.display = "none";
+    document.getElementById('VPDexhibition_line_expand').style.display = "none";
+    document.getElementById('decoNdesignPhotos').style.display = "none";
+    document.getElementById('gamesPhotos').style.display = "none";
+    document.getElementById('exhibitionPhotos').style.display = "none";
+    document.getElementById('ticketingPhotos').style.display = "none";
+
+    document.getElementById('arrowdown4').style.display = "none";
+    document.getElementById('VPDevent_line').style.display = "none";
+    document.getElementById('VPDevent_line_expand').style.display = "none";
+    document.getElementById('eventPhotos').style.display = "none";
+    document.getElementById('protocolPhotos').style.display = "none";
+    document.getElementById('hallPhotos').style.display = "none";
+    document.getElementById('logisticPhotos').style.display = "none";
+   
+    document.getElementById('webAdminTitle').style.display = "block";				   
+    document.getElementById('emceeTitle').style.display = "block";				   
+    document.getElementById('multimediaTitle').style.display = "block";
+	document.getElementById('sponsorshipTitle').style.display = "none";
+	document.getElementById('PR_Title').style.display = "none";
+	document.getElementById('publicityTitle').style.display = "none";
+    document.getElementById('decoNdesignTitle').style.display = "none";
+    document.getElementById('gamesTitle').style.display = "none";
+    document.getElementById('exhibitionTitle').style.display = "none";
+    document.getElementById('ticketingTitle').style.display = "none";
+    document.getElementById('eventTitle').style.display = "none";
+    document.getElementById('protocolTitle').style.display = "none";
+    document.getElementById('hallTitle').style.display = "none";
+    document.getElementById('logisticTitle').style.display = "none";
+	return false;		
+}
+
+function Main2() {
+    document.getElementById('VPDpublic_line').style.display = "block";
+    document.getElementById('arrowdown2').style.display = "inline";
+    document.getElementById('arrowdown2').style.marginLeft = "450px";
+
+    document.getElementById('arrowright1').style.display = "none";
+    document.getElementById('arrowright2').style.display = "none";
+    document.getElementById('arrowright3').style.display = "none";
+    document.getElementById('arrowright4').style.display = "none";
+
+    document.getElementById('arrow1').style.display = "inline";
+    document.getElementById('arrow2').style.display = "none";
+    document.getElementById('arrow3').style.display = "inline";
+    document.getElementById('arrow4').style.display = "inline";
+
+    document.getElementById('arrowdown1').style.display = "none";
+    document.getElementById('PD_line').style.display = "none";
+    document.getElementById('PD_line_expand').style.display = "none";
+    document.getElementById('webAdminPhotos').style.display = "none";
+    document.getElementById('emceePhotos').style.display = "none";
+    document.getElementById('multimediaPhotos').style.display = "none";
+
+    document.getElementById('arrowdown3').style.display = "none";
+    document.getElementById('VPDexhibition_line').style.display = "none";
+    document.getElementById('VPDexhibition_line_expand').style.display = "none";
+    document.getElementById('decoNdesignPhotos').style.display = "none";
+    document.getElementById('gamesPhotos').style.display = "none";
+    document.getElementById('exhibitionPhotos').style.display = "none";
+    document.getElementById('ticketingPhotos').style.display = "none";
+
+    document.getElementById('arrowdown4').style.display = "none";
+    document.getElementById('VPDevent_line').style.display = "none";
+    document.getElementById('VPDevent_line_expand').style.display = "none";
+    document.getElementById('eventPhotos').style.display = "none";
+    document.getElementById('protocolPhotos').style.display = "none";
+    document.getElementById('hallPhotos').style.display = "none";
+    document.getElementById('logisticPhotos').style.display = "none";
+
+    document.getElementById('webAdminTitle').style.display = "none";
+    document.getElementById('emceeTitle').style.display = "none";
+    document.getElementById('multimediaTitle').style.display = "none";
+	document.getElementById('sponsorshipTitle').style.display = "block";
+	document.getElementById('PR_Title').style.display = "block";
+	document.getElementById('publicityTitle').style.display = "block";
+    document.getElementById('decoNdesignTitle').style.display = "none";
+    document.getElementById('gamesTitle').style.display = "none";
+    document.getElementById('exhibitionTitle').style.display = "none";
+    document.getElementById('ticketingTitle').style.display = "none";
+    document.getElementById('eventTitle').style.display = "none";
+    document.getElementById('protocolTitle').style.display = "none";
+    document.getElementById('hallTitle').style.display = "none";
+    document.getElementById('logisticTitle').style.display = "none";
+	return false;
+}
+
+function Main3() { 
+    document.getElementById('VPDexhibition_line').style.display = "block";
+    document.getElementById('arrowdown3').style.display = "inline";
+    document.getElementById('arrowdown3').style.marginLeft = "450px";
+
+    document.getElementById('arrowright1').style.display = "none";
+    document.getElementById('arrowright2').style.display = "none";
+    document.getElementById('arrowright3').style.display = "none";
+    document.getElementById('arrowright4').style.display = "none";
+
+    document.getElementById('arrow1').style.display = "inline";
+    document.getElementById('arrow2').style.display = "inline";
+    document.getElementById('arrow3').style.display = "none";
+    document.getElementById('arrow4').style.display = "inline";
+
+    document.getElementById('arrowdown1').style.display = "none";
+    document.getElementById('PD_line').style.display = "none";
+    document.getElementById('PD_line_expand').style.display = "none";
+    document.getElementById('webAdminPhotos').style.display = "none";
+    document.getElementById('emceePhotos').style.display = "none";
+    document.getElementById('multimediaPhotos').style.display = "none";
+
+    document.getElementById('arrowdown2').style.display = "none";
+    document.getElementById('VPDpublic_line').style.display = "none";
+    document.getElementById('VPDpublic_line_expand').style.display = "none";
+    document.getElementById('sponsorshipPhotos').style.display = "none";
+    document.getElementById('PR_Photos').style.display = "none";
+    document.getElementById('publicityPhotos').style.display = "none";
+
+    document.getElementById('arrowdown4').style.display = "none";
+    document.getElementById('VPDevent_line').style.display = "none";
+    document.getElementById('VPDevent_line_expand').style.display = "none";
+    document.getElementById('eventPhotos').style.display = "none";
+    document.getElementById('protocolPhotos').style.display = "none";
+    document.getElementById('hallPhotos').style.display = "none";
+    document.getElementById('logisticPhotos').style.display = "none";
+
+    document.getElementById('webAdminTitle').style.display = "none";
+    document.getElementById('emceeTitle').style.display = "none";
+    document.getElementById('multimediaTitle').style.display = "none";
+	document.getElementById('sponsorshipTitle').style.display = "none";
+	document.getElementById('PR_Title').style.display = "none";
+	document.getElementById('publicityTitle').style.display = "none";
+    document.getElementById('decoNdesignTitle').style.display = "block";
+    document.getElementById('gamesTitle').style.display = "block";
+    document.getElementById('exhibitionTitle').style.display = "block";
+    document.getElementById('ticketingTitle').style.display = "block";
+    document.getElementById('eventTitle').style.display = "none";
+    document.getElementById('protocolTitle').style.display = "none";
+    document.getElementById('hallTitle').style.display = "none";
+    document.getElementById('logisticTitle').style.display = "none";
+	return false;				
+}
+
+function Main4() { 
+    document.getElementById('VPDevent_line').style.display = "block";
+    document.getElementById('arrowdown4').style.display = "inline";
+    document.getElementById('arrowdown4').style.marginLeft = "450px";
+
+    document.getElementById('arrowright1').style.display = "none";
+    document.getElementById('arrowright2').style.display = "none";
+    document.getElementById('arrowright3').style.display = "none";
+    document.getElementById('arrowright4').style.display = "none";
+
+    document.getElementById('arrow1').style.display = "inline";
+    document.getElementById('arrow2').style.display = "inline";
+    document.getElementById('arrow3').style.display = "inline";
+    document.getElementById('arrow4').style.display = "none";
+
+    document.getElementById('arrowdown1').style.display = "none";
+    document.getElementById('PD_line').style.display = "none";
+    document.getElementById('PD_line_expand').style.display = "none";
+    document.getElementById('webAdminPhotos').style.display = "none";
+    document.getElementById('emceePhotos').style.display = "none";
+    document.getElementById('multimediaPhotos').style.display = "none";
+
+    document.getElementById('arrowdown2').style.display = "none";
+    document.getElementById('VPDpublic_line').style.display = "none";
+    document.getElementById('VPDpublic_line_expand').style.display = "none";
+    document.getElementById('sponsorshipPhotos').style.display = "none";
+    document.getElementById('PR_Photos').style.display = "none";
+    document.getElementById('publicityPhotos').style.display = "none";
+
+    document.getElementById('arrowdown3').style.display = "none";
+    document.getElementById('VPDexhibition_line').style.display = "none";
+    document.getElementById('VPDexhibition_line_expand').style.display = "none";
+    document.getElementById('decoNdesignPhotos').style.display = "none";
+    document.getElementById('gamesPhotos').style.display = "none";
+    document.getElementById('exhibitionPhotos').style.display = "none";
+    document.getElementById('ticketingPhotos').style.display = "none";
+
+    document.getElementById('webAdminTitle').style.display = "none";
+    document.getElementById('emceeTitle').style.display = "none";
+    document.getElementById('multimediaTitle').style.display = "none";
+	document.getElementById('sponsorshipTitle').style.display = "none";
+	document.getElementById('PR_Title').style.display = "none";
+	document.getElementById('publicityTitle').style.display = "none";
+    document.getElementById('decoNdesignTitle').style.display = "none";
+    document.getElementById('gamesTitle').style.display = "none";
+    document.getElementById('exhibitionTitle').style.display = "none";
+    document.getElementById('ticketingTitle').style.display = "none";
+    document.getElementById('eventTitle').style.display = "block";
+    document.getElementById('protocolTitle').style.display = "block";
+    document.getElementById('hallTitle').style.display = "block";
+    document.getElementById('logisticTitle').style.display = "block";
+	return false;				
+}
+
+function DeptPD() {
+    document.getElementById('arrowdown1').style.display = "none";
+    document.getElementById('arrowright1').style.display = "block";
+    document.getElementById('PD_line').style.display = "none";
+    document.getElementById('PD_line_expand').style.display = "block";
+    document.getElementById('webAdminPhotos').style.display = "block";
+    document.getElementById('emceePhotos').style.display = "block";
+    document.getElementById('multimediaPhotos').style.display = "block";
+	return false;				
+}
+
+function DeptPD_close() {
+    document.getElementById('arrowdown1').style.display = "block";
+    document.getElementById('arrowdown1').style.marginLeft = "950px";
+    document.getElementById('arrowright1').style.display = "none";
+    document.getElementById('PD_line').style.display = "block";
+    document.getElementById('PD_line_expand').style.display = "none";
+    document.getElementById('webAdminPhotos').style.display = "none";
+    document.getElementById('emceePhotos').style.display = "none";
+    document.getElementById('multimediaPhotos').style.display = "none";
+    return false;
+}
+
+function DeptVPDpublic() {
+    document.getElementById('arrowdown2').style.display = "none";
+    document.getElementById('arrowright2').style.display = "block";
+    document.getElementById('VPDpublic_line').style.display = "none";
+    document.getElementById('VPDpublic_line_expand').style.display = "block";
+    document.getElementById('sponsorshipPhotos').style.display = "block";
+    document.getElementById('PR_Photos').style.display = "block";
+    document.getElementById('publicityPhotos').style.display = "block";
+	return false;				
+}
+
+function DeptVPDpublic_close() {
+    document.getElementById('arrowdown2').style.display = "block";
+    document.getElementById('arrowdown2').style.marginLeft = "950px";
+    document.getElementById('arrowright2').style.display = "none";
+    document.getElementById('VPDpublic_line').style.display = "block";
+    document.getElementById('VPDpublic_line_expand').style.display = "none";
+    document.getElementById('sponsorshipPhotos').style.display = "none";
+    document.getElementById('PR_Photos').style.display = "none";
+    document.getElementById('publicityPhotos').style.display = "none";
+	return false;				
+}
+
+function DeptVPDexhibition() {
+    document.getElementById('arrowdown3').style.display = "none";
+    document.getElementById('arrowright3').style.display = "block";
+    document.getElementById('VPDexhibition_line').style.display = "none";
+    document.getElementById('VPDexhibition_line_expand').style.display = "block";
+    document.getElementById('decoNdesignPhotos').style.display = "block";
+    document.getElementById('gamesPhotos').style.display = "block";
+    document.getElementById('exhibitionPhotos').style.display = "block";
+    document.getElementById('ticketingPhotos').style.display = "block";
+	return false;				
+}
+
+function DeptVPDexhibition_close() {;
+    document.getElementById('arrowdown3').style.display = "block";
+    document.getElementById('arrowdown3').style.marginLeft = "950px";
+    document.getElementById('arrowright3').style.display = "none";
+    document.getElementById('VPDexhibition_line').style.display = "block"
+    document.getElementById('VPDexhibition_line_expand').style.display = "none";
+    document.getElementById('decoNdesignPhotos').style.display = "none";
+    document.getElementById('gamesPhotos').style.display = "none";
+    document.getElementById('exhibitionPhotos').style.display = "none";
+    document.getElementById('ticketingPhotos').style.display = "none";
+	return false;				
+}
+
+function DeptVPDevent() {
+    document.getElementById('arrowdown4').style.display = "none";
+    document.getElementById('arrowright4').style.display = "block";
+    document.getElementById('VPDevent_line').style.display = "none";
+    document.getElementById('VPDevent_line_expand').style.display = "block";
+    document.getElementById('eventPhotos').style.display = "block";
+    document.getElementById('protocolPhotos').style.display = "block";
+    document.getElementById('hallPhotos').style.display = "block";
+    document.getElementById('logisticPhotos').style.display = "block";
+	return false;				
+}
+
+function DeptVPDevent_close() {
+    document.getElementById('arrowdown4').style.display = "block";
+    document.getElementById('arrowdown4').style.marginLeft = "950px";
+    document.getElementById('arrowright4').style.display = "none";
+    document.getElementById('VPDevent_line').style.display = "block";
+    document.getElementById('VPDevent_line_expand').style.display = "none";
+    document.getElementById('eventPhotos').style.display = "none";
+    document.getElementById('protocolPhotos').style.display = "none";
+    document.getElementById('hallPhotos').style.display = "none";
+    document.getElementById('logisticPhotos').style.display = "none";
+	return false;				
+}
